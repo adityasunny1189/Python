083e1a972b90ae15282bb94e18e862989ff29917
adityasunny1189 <adityapathak1189@gmail.com>
2020-05-12 00:09:28 +0530

    initial commit

---
+++ PYTHON/basics.py
@@ -0,0 +1,41 @@
+# This is a comment
+print("Hello World")
+
+# Primitive data type
+# There is no data type all are basicaly an object
+
+# Kinds of Primitive datatype
+# Integers
+# Float
+# String
+# Complex
+
+# Examples
+
+# Integer
+a = 2;
+print(type(a))
+
+
+# Float
+a = 1.3
+print(type(a))
+
+# String 
+a = "Aditya Pathak"
+print(type(a))
+
+# Complex
+a = 5j
+print(type(a))
+
+# Variable or Identifiers
+# Name can start with char and _ but not with no's
+# Eg: a, b, _ab, _a23n
+_ab90 = 45
+print(_ab90)
+
+# String Concatination
+first_name = "Aditya"
+last_name = "Pathak"
+print("Hello Mr {}, {}".format(first_name, last_name))
